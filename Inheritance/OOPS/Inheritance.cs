10603d86dd5a85d669eeb39002a718def780e3c6
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-01 18:00:55 +0530

    inheritance and overloading

---
+++ Inheritance/OOPS/Inheritance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace OopsDemo.CSOOPS
+{
+    class Vehicle  // base class (parent) 
+    {
+        public string brand = "Ford";  // Vehicle field
+        public void honk()             // Vehicle method 
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+
+    class Car : Vehicle  // derived class (child)
+    {
+        public string modelName = "Mustang";  // Car field
+    }
+
+
+}
